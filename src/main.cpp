648dadbf702172f88afec5b476b0602642f57e01
mihai.calitescu <mihaicalitescu18@gmail.com>
2021-03-10 18:19:10 +0200

    First try

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
-using std::cout;
+#include <string>
+#include <unordered_map>
+
+class ReadInput {
+
+    std::unordered_map<std::string, int> commands;
+
+public:
+
+    ReadInput() {
+        commands["xboard"] = 0;
+        commands["protover"] = 1;
+        commands["feature"] = 2;
+        commands["a2a4"] = 3;
+    }
+
+    void readInput() {
+        std::string input;
+        getline(std::cin, input);
+
+        switch (commands[input.substr(0, input.find(" "))]) {
+            case 0: {
+                std::cout << std::endl;
+                std::cout.flush();
+                break;
+            }
+            case 1: {
+                std::cout << "feature sigint=0 san=0 name=\"chess\"" << std::endl;
+                std::cout.flush();
+                break;
+            }
+            default: {
+                std::cout << "move a7a6" << std::endl;
+            }
+        }
+    }
+};
 
 int main() {
-    cout << "hikaru" << "\n";
-    return 0;
+
+    ReadInput reader;
+    reader.readInput();
+
 }
