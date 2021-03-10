f37289c0908868aace6f44ce491f3a9ded1fc08f
mihai.calitescu <mihaicalitescu18@gmail.com>
2021-03-10 18:15:43 +0200

    First try

---
+++ src/StringHelper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by longclaw on 10.03.2021.
+//
+
+#include "StringHelper.h"
---
+++ src/StringHelper.h
@@ -0,0 +1,14 @@
+//
+// Created by longclaw on 10.03.2021.
+//
+
+#ifndef CHESSENGINE_STRINGHELPER_H
+#define CHESSENGINE_STRINGHELPER_H
+
+
+class StringHelper {
+
+};
+
+
+#endif //CHESSENGINE_STRINGHELPER_H
