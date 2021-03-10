7609c4848f434dbf89da3f665a956875c86020b5
mihai.calitescu <mihaicalitescu18@gmail.com>
2021-03-10 19:53:18 +0200

    Almost done parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <unordered_map>
 
 class ReadInput {
@@ -11,27 +12,49 @@ public:
     ReadInput() {
         commands["xboard"] = 0;
         commands["protover"] = 1;
-        commands["feature"] = 2;
-        commands["a2a4"] = 3;
+        commands["new"] = 2;
+        commands["force"] = 3;
+        commands["go"] = 4;
+        commands["quit"] = 5;
     }
 
     void readInput() {
-        std::string input;
-        getline(std::cin, input);
 
-        switch (commands[input.substr(0, input.find(" "))]) {
-            case 0: {
-                std::cout << std::endl;
-                std::cout.flush();
-                break;
-            }
-            case 1: {
-                std::cout << "feature sigint=0 san=0 name=\"chess\"" << std::endl;
-                std::cout.flush();
-                break;
-            }
-            default: {
-                std::cout << "move a7a6" << std::endl;
+        while (true) {
+            std::string input;
+            getline(std::cin, input);
+
+            switch (commands[input.substr(0, input.find(" "))]) {
+                case 0: {
+                    std::cout << std::endl;
+                    std::cout.flush();
+                    break;
+                }
+                case 1: {
+                    std::cout << "feature sigint=0 san=0 name=\"chess\"" << std::endl;
+                    std::cout.flush();
+                    break;
+                }
+                case 2: {
+                    // TODO: Initialize new board
+                    break;
+                }
+                case 3: {
+                    // TODO: Stop the bot from playing and halt
+                    break;
+                }
+                case 4: {
+                    // TODO: Force the bot to play a move and continue thinking
+                    break;
+                }
+                case 5: { // end the program
+                    return;
+                }
+                default: {
+                    std::cout << "move a7a5" << std::endl;
+                    std::cout.flush();
+                    break;
+                }
             }
         }
     }
@@ -39,7 +62,5 @@ public:
 
 int main() {
 
-    ReadInput reader;
     reader.readInput();
-
 }
